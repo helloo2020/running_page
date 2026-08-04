9466b7a4abdbfc989cca18269307724cb8534a94
Tim Zheng <zenjury@gmail.com>
2026-08-04 22:05:38 +0800

    Add Scond brand next to logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ const Header = ({ actions }: { actions?: ReactNode }) => {
     <>
       <nav className="mt-4 flex w-full items-center justify-between px-4 lg:mt-12 lg:px-16">
         <div className="shrink-0">
-          <Link to={siteUrl}>
+          <Link to={siteUrl} className="flex items-center gap-2 lg:gap-3">
             <picture>
               <img
                 className="h-11 w-11 rounded-full lg:h-16 lg:w-16"
@@ -17,6 +17,9 @@ const Header = ({ actions }: { actions?: ReactNode }) => {
                 src={logo}
               />
             </picture>
+            <span className="text-xl font-bold italic text-[#f4f4f4] lg:text-2xl">
+              Scond
+            </span>
           </Link>
         </div>
         <div className="flex flex-wrap items-center justify-end gap-2 text-right lg:gap-4">
